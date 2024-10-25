2e3ff49334faba41e5302be2c11660b0297686aa
Thomas <163284055+thomas00012@users.noreply.github.com>
2024-10-25 10:15:42 +0200

    Add files via upload

---
+++ SelectionParents.py
@@ -0,0 +1,7 @@
+class SelectionParents:
+    def __init__(self, population):
+        self.population = population
+
+    def selection_parents(self):
+        """Méthode à implémenter dans les classes filles"""
+        pass
