e055d327c8393fd11c50b04bfb0b42649cf034ac
Cissy <116278801+usamippiu@users.noreply.github.com>
2024-10-25 09:29:30 +0200

    Update Individu.py

--- Individu.py
+++ Individu.py
@@ -5,7 +5,7 @@ from Coordonnee import Coordonnee
 class Individu:
     def __init__(self, coordonnees):
         self.coordonnees = coordonnees # nom des variables, liste des valeurs, codage en base prédéfinie
-        self.scorePerformance = 0 # evaluation de la fonction choisie # l'objet
+        self.scorePerformance = None # evaluation de la fonction choisie # l'objet
 
     def set_score_performance(self, population):
         self.scorePerformance = population.fonctionPerformance.eval(self.coordonnees.valeur)
