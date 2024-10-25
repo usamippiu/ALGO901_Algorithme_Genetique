0fa548789465975ffe498c7ab2c8b8a370c6af0e
Thomas <163284055+thomas00012@users.noreply.github.com>
2024-10-25 09:26:23 +0200

    Add files via upload

--- F9.py
+++ F9.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 class F9(Performance):
-    def eval(self, V, coordonees):
-        return 10 * V + sum(-x * np.sin(np.sqrt(np.abs(x))) for x in coordonees)
+    def eval(self, coordonees):
+        return 10 + sum(-x * np.sin(np.sqrt(np.abs(x))) for x in coordonees)
 
 
 if __name__ == "__main__":
