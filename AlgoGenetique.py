f32573d3671a60c49a3913d3715674d61a249bc6
thomas00012 <thomas.petellat@gmail.com>
2025-01-17 12:44:58 +0100

    debut classe AlgoGenetique

--- AlgoGenetique.py
+++ AlgoGenetique.py
@@ -1,3 +1,34 @@
+from Fenetre import Fenetre
+from Population import Population
+
+
 class AlgoGenetique:
-    def __init__(nb_iter_max):
+    def __init__(
+        self,
+        fonction,
+        ranges,
+        taille_population,
+        nb_iter_max,
+        codage,
+    ):
+        self.fonction = fonction
         self.nb_iter_max = nb_iter_max
+        self.taille_population = taille_population
+        self.fenetres = []
+        for i in range(len(ranges)):
+            self.fenetres.append(Fenetre(f"x{i}", ranges[i][0], ranges[i][1]))
+        self.codage = codage
+        self.population = Population(self.taille_population, self.fonction)
+
+        self.population.generer_population(self.fenetres, self.codage)
+        self.individu_min = self.population.lf_individu_minimal()
+
+    def get_min(self):
+        nb_iter = 0
+        while nb_iter < self.nb_iter_max:
+            pass
+
+
+if __name__ == "__main__":
+    alg = AlgoGenetique(10, 10, [[-10, 10], [-5, 5]])
+    print(alg.fenetres[0].min)
