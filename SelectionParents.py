ddab4e05db66aee4a1b919a92d1c99d732cc0442
thomas00012 <thomas.petellat@gmail.com>
2025-01-17 15:47:53 +0100

    pourquoi 2 classe Selection et SelectionParents ? puisqu'elles font la même chose

--- SelectionParents.py
+++ SelectionParents.py
@@ -1,6 +1,4 @@
 class SelectionParents:
-    def __init__(self, population):
-        self.population = population
 
     def selection_parents(self):
         """Méthode à implémenter dans les classes filles"""
