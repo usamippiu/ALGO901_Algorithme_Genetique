26fd33521b7b02b4a195787e795356a947028ab0
Meazizi <mehdi381000@hotmail.fr>
2025-01-22 16:06:29 +0100

    Ajout d'un nouveau test dans le Mutation_test.py

--- Mutation_test.py
+++ Mutation_test.py
@@ -1,15 +1,32 @@
 import unittest
 from MutationParFlipping import MutationParFlipping
+from MutationParPermutation import MutationParPermutation
 from Population import Population
 from F2 import F2
 
 
 class TestMutationMethods(unittest.TestCase):
-    def test_indidual_length(self):
+    def test_type_retour(self):
         population = Population(1, F2())
         taux_mutation = 1
-        mutation = MutationParFlipping(taux_mutation)
-        self.assertEqual(type(mutation.effectuer_mutation(population)), Population)
+        mutation_flipping = MutationParFlipping(taux_mutation)
+        self.assertEqual(
+            type(mutation_flipping.effectuer_mutation(population)), Population
+        )
+
+        mutation_permutation = MutationParPermutation(taux_mutation)
+        self.assertEqual(
+            type(mutation_permutation.effectuer_mutation(population)), Population
+        )
+
+    def test_taille_population(self):
+        population = Population(10, F2())
+        taux_mutation = 1
+        mutation_flipping = MutationParFlipping(taux_mutation)
+        self.assertEqual(
+            len(mutation_flipping.effectuer_mutation(population).individus),
+            len(population.individus),
+        )
 
 
 if __name__ == "__main__":
